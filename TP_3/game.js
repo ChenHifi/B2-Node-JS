ca0e218ac1768652e5cdfb901e2d61e9287bd8c8
Matthieu Martinez Espino <matthieu.martinez@ecole-isitech.fr>
2022-12-22 17:57:03 +0100

    commit j3

--- TP_3/game.js
+++ TP_3/game.js
@@ -1,15 +1,15 @@
 
 
 let map = ['✈',' ','◅'];
+a = 1;
+i = 10;
 
-
-
-jeu = [[map[0], map[1], map[1], map[1], map[1], map[2]],
-[map[1], map[1], map[1], map[1], map[1], map[2]]];
+jeu = [[map[0], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[2]],
+[map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[2]]];
 
 function aff(){
-    console.log(jeu[0][0],jeu[0][1],jeu[0][2],jeu[0][3],jeu[0][4],jeu[0][5]);
-    console.log(jeu[1][0],jeu[1][1],jeu[1][2],jeu[1][3],jeu[1][4],jeu[1][5]);
+    console.log(jeu[0][0],jeu[0][1],jeu[0][2],jeu[0][3],jeu[0][4],jeu[0][5],jeu[0][6],jeu[0][7],jeu[0][8],jeu[0][9],jeu[0][10]);
+    console.log(jeu[1][0],jeu[1][1],jeu[1][2],jeu[1][3],jeu[1][4],jeu[1][5],jeu[1][6],jeu[1][7],jeu[1][8],jeu[1][9],jeu[1][10]);
 }
 
 y = []
@@ -41,7 +41,7 @@ function resolve() {
       }, 1000);
     });
     
-  }
+    }
 
 function resolve1() {
     return new Promise(resolve => {
@@ -70,10 +70,38 @@ async function asyncCall1() {
     // expected output: "resolved"
   }
 
+function deplacementM(i){
+    if (i >= 1) {
+        y = jeu[0][i];
+        jeu[0][i] = jeu[0][i - 1];
+        jeu[0][i - 1] = y;
+        return i -= 1;
+    }else {
+        i = 10;
+        y = jeu[0][i];
+        jeu[0][i] = jeu[0][0];
+        jeu[0][0] = y;
+        return i;
+    }
+}
+
+function resolveM(i) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log("index : ", i);
+        resolve(asyncCallM(i));
+        }, 1000);
+    });
+}
+      
+async function asyncCallM(i) {
+    console.clear();
+    aff();
+    const nextIndex = deplacementM(i);
+    const resulte = await resolveM(nextIndex);
+}
+
+
+
+asyncCallM(i); 
 asyncCall();
-
-
-
-
-
-
