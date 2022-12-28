423c8e30ad5ab6b4d837875cc20fd52dfd5dc542
Matthieu Martinez Espino <matthieu.martinez@ecole-isitech.fr>
2022-12-28 15:16:34 +0100

    last commit

--- TP_3/game.js
+++ TP_3/game.js
@@ -1,20 +1,22 @@
 
 
-let map = ['✈',' ','◅'];
+let map = ['✈',' ','◅']; //on définis nos sprites
 a = 1;
-i = 10;
-
-jeu = [[map[0], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[2]],
-[map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[2]]];
-
-function aff(){
-    console.log(jeu[0][0],jeu[0][1],jeu[0][2],jeu[0][3],jeu[0][4],jeu[0][5],jeu[0][6],jeu[0][7],jeu[0][8],jeu[0][9],jeu[0][10]);
-    console.log(jeu[1][0],jeu[1][1],jeu[1][2],jeu[1][3],jeu[1][4],jeu[1][5],jeu[1][6],jeu[1][7],jeu[1][8],jeu[1][9],jeu[1][10]);
-}
-
+i = 11;
+id = 11;
 y = []
 
+jeu = [[map[0], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[2]],  //on définit l'emplacement de base pour chaque élément du jeu 
+[map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[1], map[2]]];  
+
+function aff(){
+    //on affiche la map complete 
+    console.log(jeu[0][0],jeu[0][1],jeu[0][2],jeu[0][3],jeu[0][4],jeu[0][5],jeu[0][6],jeu[0][7],jeu[0][8],jeu[0][9],jeu[0][10],jeu[0][11]); 
+    console.log(jeu[1][0],jeu[1][1],jeu[1][2],jeu[1][3],jeu[1][4],jeu[1][5],jeu[1][6],jeu[1][7],jeu[1][8],jeu[1][9],jeu[1][10],jeu[1][11]);
+}
+
 function deplacementB(index){
+    //coorespond au déplacement de notre avion du haut vers le bas, on se déplace entre les deux tableaux de aff()
     if (index <= 2) {
         y = jeu[index][0]
         jeu[index][0] = jeu[index+1][0]
@@ -25,6 +27,7 @@ function deplacementB(index){
 }
 
 function deplacementH(index){
+    //coorespond au déplacement de notre avion du bas vers le haut, on se déplace entre les deux tableaux de aff()
     if (index <= 2) {
         y = jeu[index][0]
         jeu[index][0] = jeu[index-1][0]
@@ -35,6 +38,7 @@ function deplacementH(index){
 }
 
 function resolve() {
+    //cette fonction a pour but de définir un temps donné avant l'appel de la fonction asynxCall1 - qui permetde donner ce coté "animé"
     return new Promise(resolve => {
       setTimeout(() => {
         resolve (asyncCall1());
@@ -44,6 +48,7 @@ function resolve() {
     }
 
 function resolve1() {
+    //cette fonction a pour but de définir un temps donné avant l'appel de la fonction asynxCall - qui permetde donner ce coté "animé"
     return new Promise(resolve => {
       setTimeout(() => {
         resolve (asyncCall());
@@ -53,12 +58,11 @@ function resolve1() {
     }
 
 async function asyncCall() {
-    console.clear();
-    aff();
+    //cette fonction est récursive et permet de faire tourner notre petite animation en boucle
+    console.clear();  //on clear la console pour 
+    aff();  //on affiche nos éléments pour mettre à jour notre affichage après le déplacement d'un des éléments du tableau aff()
     deplacementB(0);
     const result = await resolve();
-    console.log(result);
-    // expected output: "resolved"
   }
 
 async function asyncCall1() {
@@ -66,21 +70,19 @@ async function asyncCall1() {
     aff();
     deplacementH(1);
     const result = await resolve1();
-    console.log(result);
-    // expected output: "resolved"
   }
 
 function deplacementM(i){
-    if (i >= 1) {
+    if (i >= 2) {
         y = jeu[0][i];
         jeu[0][i] = jeu[0][i - 1];
         jeu[0][i - 1] = y;
         return i -= 1;
     }else {
-        i = 10;
+        i = 11;
         y = jeu[0][i];
-        jeu[0][i] = jeu[0][0];
-        jeu[0][0] = y;
+        jeu[0][i] = jeu[0][1];
+        jeu[0][1] = y;
         return i;
     }
 }
@@ -102,6 +104,40 @@ async function asyncCallM(i) {
 }
 
 
+function deplacementM1(id){
+    // dans ce cas celle-ci à pour but de faire en sorte que nos "missiles" fasses un roulement dans aff(), 
+    // en effet il ég=changent leur possiton avec l'index > que eux et se déplacent ainsi, une fois arrivé au même niveau il reveinne à leur position initiale
+    if (id >= 2) {
+        x = jeu[1][id];
+        jeu[1][id] = jeu[1][id - 1];
+        jeu[1][id - 1] = x;
+        return id -= 1;
+    }else {
+        id = 11;
+        x = jeu[1][id];
+        jeu[1][id] = jeu[1][1];
+        jeu[1][1] = x;
+        return id;
+    }
+}
+
+function resolveM1(id) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log("index : ", id);
+        resolve(asyncCallM1(id));
+        }, 1000);
+    });
+}
+      
+async function asyncCallM1(id) {
+    console.clear();
+    aff();
+    const nextIndex = deplacementM1(id);
+    const resulte = await resolveM1(nextIndex);
+}
 
 asyncCallM(i); 
 asyncCall();
+resolveM1(id)
+
